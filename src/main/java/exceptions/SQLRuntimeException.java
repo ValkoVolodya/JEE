538d22e8a3fc2a2a5294d80d1a1d1a1b3024952f
Valko Vova <valko.volodya@gmail.com>
2016-09-09 15:11:34 +0300

    Added SQL Runtime exception

---
+++ src/main/java/exceptions/SQLRuntimeException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+
+import java.sql.SQLException;
+
+public class SQLRuntimeException extends RuntimeException {
+
+    public SQLRuntimeException(SQLException e){
+        super(e);
+    }
+
+    public SQLRuntimeException(String message){
+        super(message);
+    }
+
+}
