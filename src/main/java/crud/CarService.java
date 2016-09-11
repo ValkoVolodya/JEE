f93f52cae9d4b7dd8e460e9a428a9ce25b295715
Valko Vova <valko.volodya@gmail.com>
2016-09-11 17:13:16 +0300

    Fixed via entity implementation

--- src/main/java/crud/CarService.java
+++ src/main/java/crud/CarService.java
@@ -1,81 +1,138 @@
 package crud;
 
 import connectionProvider.ConnectionProviderFactory;
+import entity.Car;
 import exceptions.SQLRuntimeException;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarService {
 
     private ConnectionProviderFactory cpf;
 
+    /**
+     * Initializes ConnectionProviderFactory
+     */
     public CarService() {
         cpf = new ConnectionProviderFactory();
     }
 
-    public void insert(String man, String model, String code){
-        try{
+    /**
+     * Insert entity in db
+     *
+     * @param car - entity to be deleted
+     */
+    public void insert(Car car) {
+        try {
             Statement statement = cpf.get().createStatement();
             String insert = String.format(
                     "INSERT INTO CARS (MANUFACTURER, MODEL, LICENSE_PLATE) " +
-                            "VALUES ('%s', '%s', '%s');",
-                    man, model, code
+                            "VALUES ('%s', '%s', '%s')",
+                    car.getManufacturer(),
+                    car.getModel(),
+                    car.getLicense_plate()
             );
             statement.executeUpdate(insert);
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw new SQLRuntimeException(e);
         }
     }
 
-    public ResultSet select(String criterium, String value){
-        try{
+    /**
+     * Select entities by given criterium
+     * @param criterium - field for search
+     * @param value - value of field
+     * @return List of results
+     */
+    public List<Car> select(String criterium, String value) {
+        try {
             Statement statement = cpf.get().createStatement();
             String select = String.format(
                     "SELECT * FROM CARS WHERE %s = '%s'",
                     criterium, value
             );
-            return statement.executeQuery(select);
-        }catch (SQLException e){
+            ResultSet rs = statement.executeQuery(select);
+            List<Car> list = new ArrayList<Car>();
+            while (rs.next()) {
+                list.add(new Car.Builder(
+                                rs.getString("MANUFACTURER"),
+                                rs.getString("MODEL")
+                        )
+                                .car_id(rs.getInt("CAR_ID"))
+                                .license_plate(rs.getString("LICENSE_PLATE"))
+                                .build()
+                );
+            }
+            return list;
+        } catch (SQLException e) {
             throw new SQLRuntimeException(e);
         }
     }
 
-    public void delete(int id){
-        try{
+    /**
+     * Delete entity from db
+     *
+     * @param car - entity for deleting
+     */
+    public void delete(Car car) {
+        try {
             Statement statement = cpf.get().createStatement();
             String delete = String.format(
-                    "DELETE FROM CARS WHERE CAR_ID = %d;", id
+                    "DELETE FROM CARS WHERE " +
+                            "MANUFACTURER = '%s' AND " +
+                            "MODEL = '%s' AND LICENSE_PLATE = '%s'",
+                    car.getManufacturer(),
+                    car.getModel(),
+                    car.getLicense_plate()
             );
             statement.execute(delete);
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new SQLRuntimeException(e);
         }
     }
 
-    public void delete(String criterium, String value){
-        try{
+    /**
+     * Delete row wih given id
+     *
+     * @param id - id of row to be deleted
+     */
+    public void delete(int id) {
+        try {
             Statement statement = cpf.get().createStatement();
             String delete = String.format(
-                    "DELETE FROM CARS WHERE %s = '%s'",
-                    criterium, value
+                    "DELETE FROM CARS WHERE CAR_ID = '%d'",
+                    id
             );
             statement.execute(delete);
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new SQLRuntimeException(e);
         }
     }
 
-    public void update(int id, String criterium, String newValue){
-        try{
+    /**
+     * Update row with same id.
+     *
+     * @param car entity with updated fields
+     */
+    public void update(Car car) {
+        try {
             Statement statement = cpf.get().createStatement();
             String update = String.format(
-                    "UPDATE CARS SET %s = '%s' WHERE car_id = %d",
-                    criterium, newValue, id
+                    "UPDATE CARS SET " +
+                            "MANUFACTURER = '%s' , " +
+                            "MODEL = '%s' , LICENSE_PLATE = '%s' " +
+                            "WHERE CAR_ID = %d",
+                    car.getManufacturer(),
+                    car.getModel(),
+                    car.getLicense_plate(),
+                    car.getCar_id()
             );
             statement.execute(update);
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new SQLRuntimeException(e);
         }
     }
